dfdacceffe4ad53e0483ea9215f0dc0749c7cbe0
Eugenia Kim <elk303@nyu.edu>
2019-07-21 19:53:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name,language)
   if language > ""
     puts "Hi, my name is #{name} and I am leanring to program in Ruby."
   else 
