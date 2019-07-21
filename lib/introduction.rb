55eb2ea54399d9580b763bc439109f5943f6fdbc
Eugenia Kim <elk303@nyu.edu>
2019-07-21 19:37:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name}" + " and I am leanring to program in #{language}."
 end
 
+introduction_with_language("Dan", "Ember.js")
+
 # def introduction_with_language_optional(name, language)
 #   if language > 0 
 #     puts "Hi, my name is #{name} and I am leanring to program in #{language}."
