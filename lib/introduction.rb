6f6f26d7810750b6e0f469319ec20dbdfd625e10
Eugenia Kim <elk303@nyu.edu>
2019-07-21 19:06:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,7 @@ def introduction(name)
 end
   
 def introduction_with_language(name, language)
+  name && language 
   puts "Hi, my name is #{name} and I am leanring to program in #{language}.\n"
 end
 
